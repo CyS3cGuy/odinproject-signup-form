35e1e231c6442daf85626d316915e2def7ec8e7b
CyS3cGuy <19325217+CyS3cGuy@users.noreply.github.com>
2025-01-15 20:33:19 +0800

    Added theme toggle CSS

--- js/toggle-brightness.js
+++ js/toggle-brightness.js
@@ -4,18 +4,21 @@ const headerLogoBlack = document.querySelector("#login-ui .black-logo");
 const headerLogoWhite = document.querySelector("#login-ui .white-logo");
 
 brightnessToggle.addEventListener("input", (event) => {
-    let isLight = brightnessToggle.checked;
-    if (isLight) {
-        root.classList.add("light");
-        root.classList.remove("dark");
+    let isDark = brightnessToggle.checked;
+    if (isDark) {
 
-        headerLogoBlack.style.visibility = "visible";
-        headerLogoWhite.style.visibility = "hidden";
-    } else {
         root.classList.add("dark");
         root.classList.remove("light");
 
         headerLogoBlack.style.visibility = "hidden";
         headerLogoWhite.style.visibility = "visible";
+
+        
+    } else {
+        root.classList.add("light");
+        root.classList.remove("dark");
+
+        headerLogoBlack.style.visibility = "visible";
+        headerLogoWhite.style.visibility = "hidden";
     }
 })
